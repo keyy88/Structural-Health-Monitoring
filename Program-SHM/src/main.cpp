d6a776b54adef04d0555a6a2bdfa7cef346b2493
Erlingga Primashudi <erlinggaprimashudi@gmail.com>
2024-05-11 22:53:46 +0700

    Update main.cpp

--- Program-SHM/src/main.cpp
+++ Program-SHM/src/main.cpp
@@ -9,10 +9,10 @@
 #define BAUDRATE    115200    // Baud rate
 
 #define SECRET_SSID "Asus_Lab Instrumentasi"
-#define SECRET_PASS "123456789"
+#define SECRET_PASS "12xxxxxx"
 
-#define SECRET_CH_ID 2260393
-#define SECRET_WRITE_APIKEY "U1139IB2WTCO2TN7"
+#define SECRET_CH_ID secret
+#define SECRET_WRITE_APIKEY "Secret"
 
 // Pulse config
 #define PWM_PIN     5         // PWM pin
