a24359f2ac017c3a0db1aa92fe2f18e3169e151e
Erlingga Primashudi <erlinggaprimashudi@gmail.com>
2024-05-11 22:52:13 +0700

    Add files via upload

---
+++ Program-SHM/src/secret.h
@@ -0,0 +1,8 @@
+// #define SECRET_SSID "Asus_Lab Instrumentasi"
+// #define SECRET_PASS "123456789"
+
+#define SECRET_SSID "Raditya"
+#define SECRET_PASS "Radit261"
+
+#define SECRET_CH_ID 2260393
+#define SECRET_WRITE_APIKEY "U1139IB2WTCO2TN7"
