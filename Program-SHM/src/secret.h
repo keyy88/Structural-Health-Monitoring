394615a6f9448aad50d48b3f31da0506f4d806a7
Erlingga Primashudi <erlinggaprimashudi@gmail.com>
2024-08-27 09:16:54 +0700

    Update secret.h

--- Program-SHM/src/secret.h
+++ Program-SHM/src/secret.h
@@ -1,8 +1,8 @@
 // #define SECRET_SSID "Asus_Lab Instrumentasi"
 // #define SECRET_PASS "123456789"
 
-#define SECRET_SSID "Raditya"
-#define SECRET_PASS "Radit261"
+#define SECRET_SSID "xx"
+#define SECRET_PASS "xx"
 
-#define SECRET_CH_ID 2260393
-#define SECRET_WRITE_APIKEY "U1139IB2WTCO2TN7"
+#define SECRET_CH_ID xx
+#define SECRET_WRITE_APIKEY "xx"
